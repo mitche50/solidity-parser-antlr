f7831313f3c49231e366372db1aa23717ac3b515
mitche50 <mitche50@gmail.com>
2022-05-12 14:51:29 -0400

    fix: handle multiple return values
    Functions can have multiple parameters and return values, duplicate the logic to handle multiple parameters in return values to handle this properly

--- src/natspec.js
+++ src/natspec.js
@@ -1,3 +1,5 @@
+'use strict';
+
 /**
  * Parse a full text comment to an object that can be easly consumed
  * @param comment full text comment
@@ -17,36 +19,45 @@ module.exports = function (comment) {
   }
 
   // split text by comment type
-  const splitComments = comment
-    .split(/@(title|author|notice|dev|param|return)/g);
+  var splitComments = comment.split(/@(title|author|notice|dev|param|return)/g);
   // let's start a map
-  const resultComments = {};
+  var resultComments = {};
 
   // iterate through all the split comments
   // start in the second result (because the first one is usually nothing)
   // then iterate each two elements, because since the split is done using
   // the natspec type
-  for (let x = 1; x < splitComments.length; x += 2) {
+  for (var x = 1; x < splitComments.length; x += 2) {
 
-    const key = splitComments[x];
-    const value = splitComments[x + 1].trim();
+    var key = splitComments[x];
+    var value = splitComments[x + 1].trim();
 
     // if the comment if type 'param' we need to extract the first word
     // which is the variable name
     if (key === 'param') {
-      const sep = value.indexOf(' ');
-      const paramName = value.substring(0, sep);
-      const paramValue = value.substring(sep + 1);
+      var sep = value.indexOf(' ');
+      var paramName = value.substring(0, sep);
+      var paramValue = value.substring(sep + 1);
 
       if (resultComments['params'] == null) {
-        resultComments['params'] = {}
+        resultComments['params'] = {};
       }
 
-      resultComments['params'][paramName] = paramValue
+      resultComments['params'][paramName] = paramValue;
+    } if (key === 'return') {
+      var sep = value.indexOf(' ');
+      var returnName = value.substring(0, sep);
+      var returnValue = value.substring(sep + 1);
+
+      if (resultComments['return'] == null) {
+        resultComments['return'] = {};
+      }
+
+      resultComments['return'][returnName] = returnValue;
     } else {
       // finally, if the value was found, update it
       resultComments[key] = value;
     }
   }
-  return resultComments
-}
+  return resultComments;
+};
